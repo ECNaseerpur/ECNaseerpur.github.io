4ef7833e4a06580ce9bbfa11158fc661bcac8b6e
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-02-21 14:53:20 +0530

    updated

---
+++ src/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Hamburger menu toggle
+    const menuToggle = document.querySelector("[data-collapse-toggle]");
+    const menuTarget = document.getElementById(menuToggle.getAttribute("data-collapse-toggle"));
+    menuToggle.addEventListener("click", function () {
+        const expanded = menuToggle.getAttribute("aria-expanded") === "true" || false;
+        menuToggle.setAttribute("aria-expanded", !expanded);
+        menuTarget.classList.toggle("hidden");
+    });
+
+
+});
+
