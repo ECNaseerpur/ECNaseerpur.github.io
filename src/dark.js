7797712eaa70f761120eb3d92ae9fd921baa8ae9
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-03-07 19:11:08 +0530

    updated

--- src/dark.js
+++ src/dark.js
@@ -1,10 +1,6 @@
 const darkMode = document.querySelector('.dark-mode');
 
-darkMode.addEventListener('click', () => {
-    if (darkMode.checked) {
-        document.body.classList.add('dark:text-white', 'dark:bg-gray-900');   
-    }
-    else {
-        document.body.classList.remove('dark:text-white', 'dark:bg-gray-900');
-    }
+darkMode.addEventListener('change', () => {
+    console.log('changed');
+    document.body.classList.toggle('dark');
 })
